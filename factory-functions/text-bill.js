f058555eef0a2ce4fa32fff617f845dc8be05780
WeHaveJoy <cnovuyo10@gmail.com>
2020-07-06 17:55:09 +0200

    new changes

--- factory-functions/text-bill.js
+++ factory-functions/text-bill.js
@@ -1,97 +1,31 @@
-function textBill() {
-    var theCallCost = 0;
-    var theSmsCost = 0;
-    var theWarningLevel = 0;
-    var theDangerLevel = 0;
+//var type = document.querySelector (".billTypeText");
+//var button = document.querySelector (".addToBillBtn");
+//var callT = document.querySelector (".callTotalOne");
+//var smsT = document.querySelector (".smsTotalOne");
+//var total = document.querySelector (".totalOne");
 
-    var callCostTotal = 0;
-    var smsCostTotal = 0;
+var callsTotal = 0;
+var smsTotal = 0;
 
-    function setCallCost(callCost) {
-        theCallCost = callCost;
-    }
-
-    function getCallCost() {
-        return theCallCost;
-    }
-
-    function setSmsCost(smsCost) {
-        theSmsCost = smsCost;
-    }
-
-    function getSmsCost() {
-        return theSmsCost;
-    }
-
-    function getTotalCost() {
-        return callCostTotal + smsCostTotal;
-    }
-
-    function makeCall() {
-        if(!hasReachedDangerLevel()){
-            callCostTotal += theCallCost;
-        }
-        
-    }
-
-    function getTotalCost() {
-        return callCostTotal + smsCostTotal;
-    }
-    function getTotalCallCost() {
-        return callCostTotal;
-    }
-    function getTotalSmsCost() {
-        return smsCostTotal;
-    }
-
-    function sendSms() {
-        if (!hasReachedDangerLevel()) {
-            smsCostTotal += theSmsCost;
-        }
-    }
-    function setWarningLevel(warningLevel) {
-        theWarningLevel = warningLevel;
-    }
-
-    function getWarningLevel() {
-        return theWarningLevel;
-    }
-
-
-    function setDangerLevel(dangerLevel) {
-        theDangerLevel = dangerLevel;
-    }
-    function getDangerLevel() {
-        return theDangerLevel;
-    }
-
-    function hasReachedDangerLevel() {
-        return getTotalCost() >= getDangerLevel();
-    }
-
-    function totalClassName() {
-        if (hasReachedDangerLevel()) {
-            return "danger";
-        }
-        if (getTotalCost() >= getWarningLevel()) {
-            return "warning";
-        }
-    }
-    return {
-        setCallCost,
-        getCallCost,
-        setSmsCost,
-        getSmsCost,
-        getTotalCost,
-        makeCall,
-        getTotalCallCost,
-        sendSms,
-        getTotalSmsCost,
-        setWarningLevel,
-        getWarningLevel,
-        setDangerLevel,
-        getDangerLevel,
-        totalClassName
-    }
-
+function totalPhoneBill (billString){
+var billItems = billString.split (",");
+var billTotal = 0;
+for (var i=0;i<billItems.length;i++){
+var billItem = billItems[i].trim ();
+if (billItem === "call"){
+billTotal += 2.75;
+callsTotal += 2.75;
 }
+else if (billItem === "sms"){
+billTotal += 0.75;
+smsTotal += 0.75;
+}
+}
+var roundedBillTotal = billTotal.toFixed (2);
+callT.innerHTML = (callsTotal).toFixed (2);
+smsT.innerHTML = (smsTotal).toFixed (2);
+return roundedBillTotal;
+}
+
+
+button.addEventListener ("click", calculateBtnClicked);
