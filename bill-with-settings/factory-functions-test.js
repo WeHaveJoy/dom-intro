ab9a828e7e60e7deb4ad96970889639af23b065d
WeHaveJoy <cnovuyo10@gmail.com>
2020-07-11 17:45:27 +0200

    new changes

---
+++ bill-with-settings/factory-functions-test.js
@@ -0,0 +1,100 @@
+function billWithSettings(){
+    var theCallCost = 0;
+    var theSmsCost =0;
+    var theWarningLevel=0;
+    var thecriticalLevel= 0;
+
+    var callCostTotal =0;
+    var smsCostTotal =0; 
+
+    function setCallCost(callCost){
+        theCallCost = callCost;
+    }
+
+    function getCallCost(){
+        return theCallCost;
+    }
+
+    function setSmsCost(){
+        theSmsCost += smsCost;
+    }
+
+    function getSmsCost(){
+        return theSmsCost;
+    }
+
+    function setWarningLevel(_warnigLevel){
+        theWarningLevel = warningLevel;
+    }
+
+    function getWarningLevel(){
+        return theWarningLevel;
+    }
+
+    function setCriticalLevel(criticalLevel){
+        theCriticalLevel = criticalLevel;
+    }
+
+    function getCriticalLevel(){
+        return thecriticalLevel;
+    }
+
+    function makeCall(){
+        callCostTotal += theCallCost;
+    }
+
+    function getTotalCost(){
+        return callCostTotal + smsCostTotal;
+    }
+
+    function getTotalCallCost(){
+        return theCallCost;
+
+    }
+
+    function getTotalSmsCost(){
+
+
+        return theSmsCost;
+    }
+
+    function sendSms(){
+
+        if(!hasReachedCriticalLevel()){
+            smsCostTotal += theSmsCost;
+        }
+        
+    }
+
+    function hasReachedCriticalLevel(){
+        return getTotalCost() >= getWarningLevel();
+    }
+
+    function totalClassName(){
+            if(hasReachedCriticalLevel()){
+                return "warning";
+            }
+
+            if(getTotalCost() >= getCriticalLevel()){
+                return "critical";
+            }
+    }
+
+
+    return{
+        setCallCost,
+        getCallCost,
+        setSmsCost,
+        getSmsCost,
+        setWarningLevel,
+        getWarningLevel,
+        setCriticalLevel,
+        getCriticalLevel,
+        makeCall,
+        getTotalCost,
+        getTotalCallCost,
+        getTotalSmsCost,
+        sendSms,
+        totalClassName
+    }
+}
